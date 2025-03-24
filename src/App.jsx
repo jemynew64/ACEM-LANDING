71d955a3a438609ca21950323c22940b9e60a68e
jemal leon <jemalxd8@gmail.com>
2025-03-24 12:00:38 -0500

    Agregar configuración inicial de Vite y React con soporte para Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-morado-1">hola mundo</h1>
+      <h1 className="bg-morado-2">hola mundo</h1>
+      <h1 className="bg-morado-3">hola mundo</h1>
+      <h1 className="">hola mundo</h1>
+    </>
+  );
+}
+
+export default App;
